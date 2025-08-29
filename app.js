7ee0691dc47411dd9bc93c945d9d84f9d7302382
RITTICK DUTTA <rittickdutt369@gmail.com>
2025-08-29 19:22:40 +0530

    code for the file creation is added

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 import readline from "readline"
+import chalk from "chalk"
+import fs from "fs"
 const rl=readline.createInterface({
     input:process.stdin,
     output:process.stdout
 })
+
+const filecreation=()=>{
+    rl.question(chalk.blue("Enter Your Filename: "),(filename)=>{
+        rl.question(chalk.blue("Enter the content :"),(content)=>{
+            fs.writeFile(`${filename}.txt`,content,(err)=>{
+                if(err){
+                    console.error(chalk.red(err));
+                    rl.close();
+                }
+                else{console.log(chalk.green("file created successfully"));
+                    rl.close();
+                    
+                }
+
+            })
+        })
+    })
+
+}
+
+
+filecreation();
